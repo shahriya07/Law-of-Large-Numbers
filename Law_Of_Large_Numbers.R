ef91359bf6c5277ce89b9502c4f9b1cac9d38745
Riya Shah <shahriya199907@gmail.com>
2020-03-18 18:39:56 -0400

    documenting code

--- Law_Of_Large_Numbers.R
+++ Law_Of_Large_Numbers.R
@@ -1,13 +1,23 @@
 
+
+#Specifying sample size
 N <- 1000
+
 counter <- 0
+
+# iterating over vector of numbers
 for(i in rnorm(N)){
+  # condition for the number between -1 to 1
   if(i <= 1 & i >= -1){
     counter <- counter + 1
   }
 }
+
+# calculating hit-ratio
 answer <- counter / N
 answer
+
+# print if the hit-radio is equal to the probability
 if(answer == 0.682){
   print("Solved!!!")
 }
